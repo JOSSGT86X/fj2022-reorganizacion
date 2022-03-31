0959b5b69d8f32707b3c1864d309dad5b4da3d23
Jose Humberto Gutierrez Montoya <19030681@uadeo.mx>
2022-03-31 08:53:20 -0700

    C12

--- src/User.php
+++ src/User.php
@@ -1,1 +1,14 @@
 <?php
+
+user /mysql;
+
+class mysql extends \mysqli {
+    protected $data = [
+    'host' => '127.0.0.1',
+    'user' => 'root',
+    'password' => '',
+    'dbname' => 'uadeo',
+    'port' => 1386  
+
+    ];
+}
